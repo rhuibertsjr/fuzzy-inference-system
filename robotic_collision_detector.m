fcdb6e81208fc5e6addc2edf8ef77af44d049e9b
rhuibertsjr <rhuibertsjr@gmail.com>
2024-01-09 15:03:30 +0100

    Added: membership functions + extra linguistic variables

--- robotic_collision_detector.m
+++ robotic_collision_detector.m
@@ -8,12 +8,31 @@
 #
 
 ## rhjr: Create Fuzzy Inference System.
-fuz = newfis("path_finder")
+fis = newfis("path_finder");
 
-## rhjr: adding membership functions.
-fuz = addvar(fuz, "input", "speed",  "trapmf", [ ])
+## rhjr: adding membership functions (mf):
+##         - trapezoid 
+##         - triangle    
+fis = addvar(fis, "input", "speed",                 [-0.1 1.1]);
+fis = addmf(fis, "input", 1, "slow",     "trapmf", [-0.1 0.0 0.3 0.4]);
+fis = addmf(fis, "input", 1, "moderate", "trimf",  [0.2 0.6 0.8]);
+fis = addmf(fis, "input", 1, "fast",     "trapmf", [0.7 0.9 1.0 1.1]);
 
+fis = addvar(fis, "input", "obstacle_density", [-0.1 1.1]);
+fis = addmf(fis, "input", 2, "sparse",   "trapmf", [-0.1 0.0 0.5 0.6]);
+fis = addmf(fis, "input", 2, "moderate", "trimf",  [0.5 0.6 0.8]);
+fis = addmf(fis, "input", 2, "dense",    "trapmf", [0.8 0.9 1.0 1.1]);
 
+## rhjr: output membership function
+fis = addvar(fis, "output", "status", [0.0 1.0]);
+fis = addmf(fis, "output", 1, "ok",      "constant", 0.4);
+fis = addmf(fis, "output", 1, "warning", "constant", 0.7);
+fis = addmf(fis, "output", 1, "crashed", "constant", 0.9);
+
+## rhjr: visualize membership functions
+plotmf (fis, "input",  1);
+plotmf (fis, "input",  2);
+plotmf (fis, "output", 1);
 
 ### References
 ##    -  
